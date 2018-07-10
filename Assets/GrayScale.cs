481239126ebaeebf9cfcd229ea6445a2bd4f1f30
douduck08 <lu10082000@gmail.com>
2018-07-11 03:00:09 +0800

    add grayscale effect

---
+++ Assets/GrayScale.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Camera)), ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class GrayScale : MonoBehaviour {
+
+    Material material;
+
+    void Start () {
+        material = new Material (Shader.Find ("Hidden/GrayScale"));
+    }
+
+    void OnRenderImage (RenderTexture src, RenderTexture dest) {
+        Graphics.Blit (src, dest, material, 0);
+    }
+}
